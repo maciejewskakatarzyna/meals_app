62b5694e54ea396854fcab75323076fd074cde0f
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-03-02 21:32:40 +0100

    add 'delete meal' button

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ import Card from 'components/Card/Card';
 export const MealsContext = React.createContext({
   meals: [],
   handleAddMeal: () => {},
+  deleteMeal: () => {},
 });
 
 const App = () => {
   const [isCardOpen, setCard] = useState(false);
   const [isFormOpen, setForm] = useState(false);
   const [meals, setMeals] = useState(mealsData);
+
+  const deleteMeal = name => {
+    const filteredMeals = meals.filter(meal => meal.name !== name);
+    setMeals(filteredMeals);
+  };
 
   const toggleForm = () => {
     isFormOpen ? setForm(false) : setForm(true);
@@ -36,7 +42,7 @@ const App = () => {
 
   return (
     <>
-      <MealsContext.Provider value={{ meals, handleAddMeal }}>
+      <MealsContext.Provider value={{ meals, handleAddMeal, deleteMeal }}>
         <Header toggleForm={toggleForm} />
         <List toggleCard={toggleCard}></List>
         {isCardOpen && <Card toggleCard={toggleCard}></Card>}
--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './ListItem.module.scss';
+import { MealsContext } from 'App';
 
-const ListItem = ({ name, image, ...props }) => (
-  <li className={styles.listItem} {...props}>
-    <img className={styles.listItemImg} src={image} alt={name} />
-    <div className={styles.listItemTitle}>
-      <p>{name}</p>
-    </div>
-  </li>
-);
+const ListItem = ({ name, image, ...props }) => {
+  const { deleteMeal } = useContext(MealsContext);
+
+  return (
+    <li className={styles.listItem} {...props}>
+      <img className={styles.listItemImg} src={image} alt={name} />
+      <div className={styles.listItemTitle}>
+        <p>{name}</p>
+        <button onClick={() => deleteMeal(name)}>x</button>
+      </div>
+    </li>
+  );
+};
 
 export default ListItem;
